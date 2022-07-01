bcca94bc17b0a65efd24d0ccdcd68106805135cd
Louisa HAFSI <lhafsi@e2r7p13.42.fr>
2022-07-01 17:55:52 +0200

    ok

--- srcs/parsing/check_map_utils.c
+++ srcs/parsing/check_map_utils.c
@@ -6,7 +6,7 @@
 /*   By: lhafsi <lhafsi@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/29 00:13:47 by lhafsi            #+#    #+#             */
-/*   Updated: 2022/06/30 15:27:15 by lhafsi           ###   ########.fr       */
+/*   Updated: 2022/07/01 17:03:50 by lhafsi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,23 +29,20 @@ int	ft_check_wall(char *line)
 int	ft_check_mid(char *line)
 {
 	int	i;
-	int	last;
-	
-	last = ft_strlen(line);
+
 	if (line[0] != '1')
 		return (0);
 	i = 1;
-	while (line[i] && i < last - 1)
+	while (line[i + 1])
 	{	
-		// printf("i = %i\nline[i] = %c\n", i, line[i]);
 		if (!(line[i] == 'C' || line[i] == 'P' || line[i] == 'E'
 			|| line[i] == '0' || line[i] == '1'))
 			return (0);
 		printf("i = %i\nline[i] = %c\n", i, line[i]);
 		i++;
 	}
-	if (line[last] != '1')
-			return (0);
+	if (line[i] != '1')
+		return (0);
 	return (1);
 }
 
